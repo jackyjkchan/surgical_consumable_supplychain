33c768c185b945777edf6dab7c5b34e9b4f8a061
jackyjkchan <jacky@DESKTOP-D69KHEB.localdomain>
2019-08-03 15:31:39 -0400

    batch script for mdp model data collection done

--- scripts/optimization_model/j_function_comparison.py
+++ scripts/optimization_model/j_function_comparison.py
@@ -3,66 +3,106 @@ import plotly
 import pacal
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import itertools
+import pandas as pd
 
+
+inv_pos_max = 30
+t_max = 15
+n_max = 5
+
+rv_0 = pacal.ConstDistr(0)
+rv_5_5 = pacal.DiscreteDistr([5], [1])
+rv_3_7 = pacal.DiscreteDistr([3, 7], [0.5, 0.5])
+rv_1_9 = pacal.DiscreteDistr([1, 9], [0.5, 0.5])
+rv_0_10 = pacal.DiscreteDistr([0, 10], [0.5, 0.5])
+
+test_cases = {
+    "E[Demand] 5": rv_5_5,
+    "E[Demand] 3, 7": rv_3_7,
+    "E[Demand] 1, 9": rv_1_9,
+    "E[Demand] 0, 10": rv_0_10
+}
 
 gamma = 0.9
 lead_time = 0
-horizon = 2
-info_state_rvs = [pacal.ConstDistr(0),
-                  pacal.ConstDistr(0),
-                  pacal.DiscreteDistr([1, 2, 3, 4], [0.25, 0.25, 0.25, 0.25])]
-
-info_state_rvs = [pacal.DiscreteDistr([1, 2, 3, 4], [0.25, 0.25, 0.25, 0.25]),
-                  pacal.ConstDistr(0),
-                  pacal.ConstDistr(0)]
-
 holding_cost = 1
 backlogging_cost = 10
 setup_cost = 5
 unit_price = 0
 
-model = StationaryOptModel(gamma,
-                           lead_time,
-                           horizon,
-                           info_state_rvs,
-                           holding_cost,
-                           backlogging_cost,
-                           setup_cost,
-                           unit_price)
+results = pd.DataFrame(columns=['exogenous_label',
+                                'information_horizon',
+                                'lead_time',
+                                't',
+                                'inventory_position_state',
+                                'information_state',
+                                'j_value_function',
+                                'base_stock',
+                                'order_up_to'])
+for case in test_cases:
+    info_rv = test_cases[case]
+    info_vals = [diracs.a for diracs in info_rv.get_piecewise_pdf().getDiracs()]
+    for n in range(n_max+1):
+        print(case, n)
+        if n == 0:
+            info_rv_vector = [info_rv, rv_0]
+            horizon = 1
+            info_states = [(0,)]
+        else:
+            info_rv_vector = [rv_0] * n + [info_rv]
+            horizon = n
+            info_states = [tuple(state) for state in itertools.product(info_vals, repeat=n)]
 
-t = 3
-o = (3, 2)
-v_values = []
-j_values = []
-max_inv_pos = 25
-#y_argmin = model.v_function_argmin(t, o)
+        model = StationaryOptModel(gamma,
+                                   lead_time,
+                                   horizon,
+                                   info_rv_vector,
+                                   holding_cost,
+                                   backlogging_cost,
+                                   setup_cost,
+                                   unit_price)
 
-# for y in range(max_inv_pos):
-#     v_values.append(model.v_function(t, y, o))
+        for t in range(t_max+1):
+            for o in info_states:
+                for x in range(inv_pos_max+1):
+
+                    j_value = model.j_function(t, x, o)
+                    base_stock = model.base_stock_level(t, o)
+                    stock_up = model.stock_up_level(t, o)
+
+                    result = {'exogenous_label': case,
+                              'information_horizon': n,
+                              'lead_time': 0,
+                              't': t,
+                              'inventory_position_state': x,
+                              'information_state': o,
+                              'j_value_function': j_value,
+                              'base_stock': base_stock,
+                              'order_up_to': stock_up
+                              }
+                    results = results.append(result, ignore_index=True)
+
+results.to_csv("optimization_model_results.csv")
+results.to_pickle("optimization_model_results.pickle")
+
+
+#     print(x)
+#     j_values.append(model.j_function(t, x, o))
 #
-# v_function_trace = go.Scatter(
+# traces[case] = go.Scatter(
 #     x=list(range(max_inv_pos)),
-#     y=v_values,
-#     name="v_function: (action, info_state) -> expected value"
+#     y=j_values,
+#     name=case
 # )
-
-for x in range(max_inv_pos):
-    print(x)
-    j_values.append(model.j_function(t, x, o))
-
-j_function_trace = go.Scatter(
-    x=list(range(max_inv_pos)),
-    y=j_values,
-    name="j_function: (inv_pos, info_state) -> expected value"
-)
-
-layout = go.Layout(title="Expected Cost",
-                   xaxis={'title': 'Inventory Position'},
-                   yaxis={'title': 'Expected Cost'})
-
-figure = go.Figure(
-    data=[j_function_trace],
-    layout=layout
-)
-
-plot(figure, filename="Expected_Cost_t={0}_o={1}.html".format(str(t), str(o)))
+#
+# layout = go.Layout(title="Value Function J",
+#                    xaxis={'title': 'Inventory Position'},
+#                    yaxis={'title': 'Optimal Expected Cost'})
+#
+# figure = go.Figure(
+#     data=[traces[case] for case in traces],
+#     layout=layout
+# )
+#
+# plot(figure, filename="Expected_Cost_Comparison_T={0}.html".format(str(t)))
