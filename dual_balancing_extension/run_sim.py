a77344749b6353db8968c4445215357aecce140f
jackyjkchan <jacky@DESKTOP-D69KHEB.localdomain>
2021-06-26 20:25:12 -0700

    script for compute canada run

---
+++ dual_balancing_extension/run_sim.py
@@ -0,0 +1,65 @@
+from scm_optimization.integer_dual_balancing import DualBalancing
+from random import random
+from scm_optimization.model import *
+from scipy.optimize import minimize, bisect, minimize_scalar
+from dual_balancing_extension.simulation import Hospital
+import pandas as pd
+import pickle
+import sys
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    rep = sys.argv[1] if len(sys.argv) > 1 else 0
+
+    backlogging_cost = 1000
+    info = 1
+
+    lead_time = 0
+    info = 0
+
+    results = pd.DataFrame()
+
+    holding_cost = 1
+    setup_cost = 0
+    unit_price = 0
+    gamma = 1
+
+    usage_model = PoissonUsageModel(scale=1)
+
+
+    results_fn = "db_results_b_{}_{}_r_{}.csv".format(str(backlogging_cost),
+                                                      str(info),
+                                                      str(rep)
+                                                      )
+    info_state_rvs = [pacal.ConstDistr(0)] * info + \
+                     [pacal.BinomialDistr(10, 0.5)]
+
+    model = DualBalancing(gamma,
+                          lead_time,
+                          info_state_rvs,
+                          holding_cost,
+                          backlogging_cost,
+                          setup_cost,
+                          unit_price,
+                          usage_model=usage_model)
+
+    print("backlogging cost:", backlogging_cost, " info: ", info, " rep: ", rep)
+
+    hospital = Hospital(db_model=model, periods=20)
+    hospital.run()
+
+    results = results.append({
+        "info": info,
+        "rep": rep,
+        "cost": hospital.cost_incurred,
+        "backlog_cost_incurred": hospital.backlog_cost_incurred,
+        "holding_cost_incurred": hospital.holding_cost_incurred,
+        "schedule": hospital.schedule,
+        "order_cont": hospital.order_continuous,
+        "order": hospital.order,
+        "demand": hospital.demand,
+        "inventory": hospital.inventory_level
+    }, ignore_index=True)
+    results.to_csv(results_fn)
+
