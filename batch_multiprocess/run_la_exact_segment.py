d02f48a54207baa64922f20a44ea6c9ca8e70ae2
jackyjkchan <jacky@DESKTOP-D69KHEB.localdomain>
2022-03-26 21:42:29 -0700

    setup cost for la

--- batch_multiprocess/run_la_exact_segment.py
+++ batch_multiprocess/run_la_exact_segment.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run LA Experiment parallel runners.')
     parser.add_argument('--outdir', dest='outdir', help='output dir to write in')
     parser.add_argument('-b', dest='backlogging_cost', type=int, help='backlogging cost')
+    parser.add_argument('-k', dest='setup_cost', type=int, help='setup cost')
     parser.add_argument('--info', dest='info',  type=int, help='info horizon')
     parser.add_argument('--binom_usage_n', dest='binom_usage_n', type=int, help='binom_usage_n')
     parser.add_argument('--pools', dest='pools', type=int, help='num of parallel runners')
@@ -28,13 +29,14 @@ if __name__ == "__main__":
 
     outdir = args.outdir if args.outdir else "."
     backlogging_cost = args.backlogging_cost if args.backlogging_cost else 1000
+    setup_cost = args.setup_cost if args.setup_cost else 0
     info = args.info if args.info else 0
     binom_usage_n = args.binom_usage_n if args.binom_usage_n else 0
     pools = args.pools if args.pools else 2
     pool_num = args.pool_num if args.pool_num else 0
     t = args.t if args.t else 0
 
-    prefix = "LA_Model_b_{}_info_{}".format(backlogging_cost, info)
+    prefix = "LA_Model_b_{}_k_{}_info_{}".format(backlogging_cost, setup_cost, info)
     prefix += "_binomial_usage_{}".format(binom_usage_n) if binom_usage_n else ""
     fn = outdir + '/' + prefix
     model = None
